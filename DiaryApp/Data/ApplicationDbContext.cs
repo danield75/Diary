9abc2cf893c9a92024ceb9db8d19140f404af582
Daniel Drion <daniel.drion@telenet.be>
2024-08-28 11:49:05 +0200

    Added seeding data for DiaryEntry

--- DiaryApp/Data/ApplicationDbContext.cs
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -11,5 +11,34 @@ namespace DiaryApp.Data
         }
 
         public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DiaryEntry>().HasData(
+                    new DiaryEntry
+                    {
+                        Id = 1,
+                        Title = "Went hiking",
+                        Content = "Went hiking with Joe!",
+                        Created = DateTime.Now
+                    },
+                    new DiaryEntry
+                    {
+                        Id = 2,
+                        Title = "Went shopping",
+                        Content = "Went shopping with Joe!",
+                        Created = DateTime.Now
+                    },
+                    new DiaryEntry
+                    {
+                        Id = 3,
+                        Title = "Went diving",
+                        Content = "Went diving with Joe!",
+                        Created = DateTime.Now
+                    }
+                );
+        }
     }
 }
