53cfd2511c6cda8734296264950f05cce6b35b08
Daniel Drion <daniel.drion@telenet.be>
2024-08-29 14:55:20 +0200

    Added client side validation for the create view of Diary Entry

--- DiaryApp/Models/DiaryEntry.cs
+++ DiaryApp/Models/DiaryEntry.cs
@@ -6,7 +6,9 @@ namespace DiaryApp.Models
     {
         //[Key]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter a title!")]
+        [StringLength(100, MinimumLength = 3, 
+            ErrorMessage = "Title must be between 3 and 100 characters!")]
         public string Title { get; set; } = string.Empty;
         [Required]
         public string Content { get; set; } = string.Empty;
